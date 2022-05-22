56465a3d05db950e5b3310a693b43e2bd0da2db7
Edilson Silva <eddysilva1994@gmail.com>
2022-05-22 01:52:52 -0300

    test: Add product enable action business rule

---
+++ tests/product_test.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from src.application.product import Product
+from src.application.status_enum import StatusEnum
+
+
+class ProductTest(TestCase):
+    def test_product_should_be_enabled_with_price_greater_than_zero(self):
+        product = Product(id=1, name="Acer VX5", price=10)
+        product.enable()
