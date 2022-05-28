2539f44d6df2a822275c80faf540c361f3efd015
Edilson Silva <eddysilva1994@gmail.com>
2022-05-28 01:11:44 -0300

    test: Apply product_mock and persistence_mock fixtures to product service disable test

--- tests/product_service_test.py
+++ tests/product_service_test.py
@@ -50,16 +50,14 @@ def test_product_service_enable_should_return_a_product(product_mock, persistenc
     assert product_mock == result
 
 
-def test_product_service_disable_should_return_a_product():
-    # Define product mock
-    product = Mock(spec=ProductInterface, name="product")
-    product.disable.return_value = None
+def test_product_service_disable_should_return_a_product(
+    product_mock, persistence_mock
+):
+    product_mock.disable.return_value = None
 
-    # Defining product persistence service interface mock and setting mocked get method return value
-    persistence = Mock(spec=ProductPersistenceServiceInterface, name="persistence")
-    persistence.save.return_value = product
+    persistence_mock.save.return_value = product_mock
 
-    service = ProductService(persistence=persistence)
-    result = service.disable(product=product)
+    service = ProductService(persistence_mock)
+    result = service.disable(product_mock)
 
-    assert product == result
+    assert product_mock == result
