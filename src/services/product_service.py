d5192034bbafd3f41f8879ec78d2ef5399170051
Edilson Silva <eddysilva1994@gmail.com>
2022-05-28 00:20:37 -0300

    feat: Add product service create business rule

--- src/services/product_service.py
+++ src/services/product_service.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from src.application.product import Product
 from src.application.product_interface import ProductInterface
 from src.services.product_persistence_service_interface import (
     ProductPersistenceServiceInterface,
@@ -19,3 +20,20 @@ class ProductService:
             Optional[ProductInterface]: Found product or None
         """
         return self.persistence.get(id)
+
+    def create(self, name: str, price: float) -> Optional[ProductInterface]:
+        """Create a product
+
+        Args:
+            name (str): Product name
+            price (float): Product price
+
+        Returns:
+            Optional[ProductInterface]: Created product or None
+        """
+        try:
+            product = Product(name=name, price=price)
+            product.is_valid()
+            return self.persistence.save(product=product)
+        except ValueError:
+            return None
