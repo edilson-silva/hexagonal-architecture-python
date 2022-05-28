5220761a26a492a613f9923fdeaa64707a9129dd
Edilson Silva <eddysilva1994@gmail.com>
2022-05-28 00:28:12 -0300

    test: Add product service create test

--- tests/product_service_test.py
+++ tests/product_service_test.py
@@ -18,3 +18,17 @@ def test_product_service_get_should_return_a_product():
     result = service.get(id="abc")
 
     assert product == result
+
+
+def test_product_service_create_should_return_a_product():
+    # Define product mock
+    product = Mock(spec=ProductInterface, name="product")
+
+    # Defining product persistence service interface mock and setting mocked get method return value
+    persistence = Mock(spec=ProductPersistenceServiceInterface, name="persistence")
+    persistence.save.return_value = product
+
+    service = ProductService(persistence=persistence)
+    result = service.create(name="Product 1", price=10)
+
+    assert product == result
